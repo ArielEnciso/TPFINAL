455b86727df981bc68830236ffd2ce5bdc9310cb
Lucas Ocampos <lucasocamp@hotmail.com>
2020-12-02 21:00:00 -0300

    Se crean Controladores y Vistas para Pacientes

---
+++ TPFINAL/Controllers/PacientesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TPFINAL.Models;
+
+namespace TPFINAL.Controllers
+{
+    [Authorize]
+    public class PacientesController : Controller
+    {
+        private HospitalEntities db = new HospitalEntities();
+
+        // GET: Pacientes
+        public ActionResult Index()
+        {
+            return View(db.Pacientes.ToList());
+        }
+
+        // GET: Pacientes/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pacientes pacientes = db.Pacientes.Find(id);
+            if (pacientes == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pacientes);
+        }
+
+        // GET: Pacientes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Pacientes/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "AseguradoNº,Nombre,Apellido,Domicilio,Ciudad,Telefono,Nº_HistorialClinico,Sexo")] Pacientes pacientes)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Pacientes.Add(pacientes);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(pacientes);
+        }
+
+        // GET: Pacientes/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pacientes pacientes = db.Pacientes.Find(id);
+            if (pacientes == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pacientes);
+        }
+
+        // POST: Pacientes/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "AseguradoNº,Nombre,Apellido,Domicilio,Ciudad,Telefono,Nº_HistorialClinico,Sexo")] Pacientes pacientes)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pacientes).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(pacientes);
+        }
+
+        // GET: Pacientes/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pacientes pacientes = db.Pacientes.Find(id);
+            if (pacientes == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pacientes);
+        }
+
+        // POST: Pacientes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Pacientes pacientes = db.Pacientes.Find(id);
+            db.Pacientes.Remove(pacientes);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
